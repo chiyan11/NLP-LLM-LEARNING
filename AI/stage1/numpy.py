6af3a4c53e17e877739df6d55f7a4fe87ce4b862
chiyan11 <q1656451519@163.com>
2026-07-30 22:26:07 +0800

    update

--- AI/stage1/numpy.py
+++ AI/stage1/numpy.py
@@ -1,4 +1,20 @@
 import numpy as np
-array = np.array([[1,2,3],[4,5,6],[7,8,9]])
+array = np.array([[1,2,3],
+                  [4,5,6],
+                  [7,8,9]],dtype=np.int64)
 print(array)
+print('array of type:',array.dtype)
+print('number of dimensions:', array.ndim)
+print('number of shape:', array.shape)
+print('number of elements:', array.size)
 
+a = np.zeros((3,3))
+b = np.empty((3,3))
+c = np.arange(10,20,2)
+d = np.arange(6).reshape(2,3)
+e = np.linspace(1,10,6).reshape(2,3)
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
